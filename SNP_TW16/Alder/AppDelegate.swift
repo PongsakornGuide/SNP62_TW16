c220ed42407d8c64bf3c3c228a63e407a85d145a
PongsakornGuide <skymanhd@hotmail.com>
2020-05-27 12:36:17 +0700

    Update CreatorView, ShowCreatorView
    Cr.SkyManHD

--- SNP_TW16/Alder/AppDelegate.swift
+++ SNP_TW16/Alder/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-//      window?.rootViewController = tabBarViewController()
+//      window?.rootViewController = LoginViewController()
         window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
         return true
     }
